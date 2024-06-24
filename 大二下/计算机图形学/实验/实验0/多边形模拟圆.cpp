ac70bc786f0f616e05b140f29c7022e3337c956c
YUKIPEDIA <123065412+YUK1PEDIA@users.noreply.github.com>
2024-06-24 10:56:53 +0800

    Add files via upload

---
+++ 大二下/计算机图形学/实验/实验0/多边形模拟圆.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+//#include <GLUT/glut.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+const int n = 10;
+const GLfloat R = 0.5f;
+const GLfloat Pi = 3.1415926536f;
+void MyDisplay()
+{
+    int i;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < n; i++)
+        glVertex2f(R * cos(2 * Pi / n * i), R * sin(2 * Pi / n * i));
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("opengl³ÌÐò");
+    glutDisplayFunc(&MyDisplay);
+    glutMainLoop();
+    return 0;
+}
